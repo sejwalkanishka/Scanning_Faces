55b1fa002ecc4da21c1c0362a8f1dfe8e9d3ffe3
Kanishka Sejwal <sejwalkanishka20@gmail.com>
2025-11-08 20:59:43 +0530

    Create Download_models.sh

---
+++ Download_models.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Download ArcFace ONNX model
+wget -O models/arcface.onnx https://path/to/arcface_onnx_model
+
+# Download RetinaFace ONNX model
+wget -O models/retinaface.onnx https://path/to/retinaface_onnx_model
